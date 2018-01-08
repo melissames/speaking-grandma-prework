47c40cfe511a6773fdd6ef894b1617bca177273b
melissames <melissa.sames@gmail.com>
2018-01-08 03:18:26 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,9 +13,9 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(words)
-  if(words == "I LOVE YOU GRANDMA")
+  if words == "I LOVE YOU GRANDMA"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif(words == words.upcase)
+  elsif words == words.upcase
     puts "NO, NOT SINCE 1938!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
