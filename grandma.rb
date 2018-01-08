f1598fbc3def7f3c34be822f9643ad58e43ad00d
melissames <melissa.sames@gmail.com>
2018-01-08 03:25:11 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,10 +14,10 @@
 
 def speak_to_grandma(words)
   if words == "I LOVE YOU GRANDMA!"
-    print "I LOVE YOU TOO PUMPKIN!"
+    "I LOVE YOU TOO PUMPKIN!"
   elsif words == words.upcase
-    print "NO, NOT SINCE 1938!"
+    "NO, NOT SINCE 1938!"
   else
-    print "HUH?! SPEAK UP, SONNY!"
-  end  
+    "HUH?! SPEAK UP, SONNY!"
+  end
 end
